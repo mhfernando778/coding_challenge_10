624d09e6a2b23d6697a587306205045eefe4d786
Fernando <mhfernando778@gmail.com>
2025-02-22 22:40:20 -0500

    Task 1 - added comments

--- cc_10.js
+++ cc_10.js
@@ -1,22 +1,22 @@
 // Task 1 - Creating a Product Class //
-class Product {
+class Product { // starting a new class for products
     constructor(name, id, price, stock) {
         this.name = name;
         this.id = id;
         this.price = price;
         this.stock = stock;
     };
-    getDetails() {
+    getDetails() { // adding a method to list all the product information
         return `Product Name: ${this.name}, Product ID: ${this.id}, Price: $${this.price}, Stock: ${this.stock}`
     };
 
-    updateStock(quantity) {
+    updateStock(quantity) { // adding a method to keep track to stock after orders are placed
         return this.stock >= quantity ? this.stock -= quantity : `Order exceeds stock. Current Stock: ${this.stock}`
     };
 }
 
-const prod1 = new Product("Laptop", 101, 1200, 10);
-console.log(prod1.getDetails()); 
+const prod1 = new Product("Laptop", 101, 1200, 10); // introducing an instance into the class
+console.log(prod1.getDetails()); // logging the details of that instance
 
-prod1.updateStock(3);
-console.log(prod1.getDetails()); 
+prod1.updateStock(3); // an order being made
+console.log(prod1.getDetails()); // logging the updated stock after the purchase
